0177ca2d6ce65d7a17de745e868c588d0b310be9
abcd <alfredbate@yahoo.co.uk>
2021-03-18 18:31:11 -0500

    Committing my first changes.

---
+++ eclipse-workspace/ArithemeticOperators/src/demo/ArithemeticOperators.java
@@ -0,0 +1,28 @@
+package demo;
+
+public class ArithemeticOperators {
+
+	public static void main(String[] args) {
+		
+		
+		int a = 10;
+		
+		int b = 20;
+		
+		int c = 30;
+		
+		System.out.println(a*b);
+		
+		System.out.println(a-b);
+		
+		System.out.println(a/b);
+		
+		System.out.println(c-a);
+		
+		
+		
+		
+
+	}
+
+}
